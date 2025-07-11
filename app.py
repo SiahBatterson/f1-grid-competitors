eef69bca611a9056e6d6437e5502e3325ef22eac
SiahBatterson <51545507+SiahBatterson@users.noreply.github.com>
2025-07-11 09:38:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
 from flask_cors import CORS
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
 @login_manager.user_loader
 def load_user(user_id):
