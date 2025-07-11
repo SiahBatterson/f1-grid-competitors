78405edeff720620b6a885d371a391c221a4ece8
SiahBatterson <51545507+SiahBatterson@users.noreply.github.com>
2025-07-11 09:14:00 -0400

    Update app.py 2

--- app.py
+++ app.py
@@ -37,6 +37,9 @@ db.init_app(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
+from flask_cors import CORS
+CORS(app)
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
